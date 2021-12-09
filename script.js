d475448cdcb166cbcc21766db002d2ac0445e63d
Digga <nagibatordier@gmail.com>
2021-12-09 20:28:17 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let names = []
+var x = true
+
+for (let i = 0; x == true; i++) {
+   
+    let comand = prompt('Введите команду')
+    let comands = comand.split(', ')
+    if (comands[0] == 'add') {
+        names.push({
+            name: comands[1]
+        })
+        comands.pop()
+        comands.pop()
+    } else if (comands[0] == 'del') {
+        names.pop()
+        comands.pop()
+        comands.pop()
+    }
+    if (comand == 'stop') {
+        var x = false
+    }
+}
+
+console.log(names);
+
+
